dd62d5c6b4ba42e0ab7845f1883f686260b95dba
Pixy <73171585+AkioHamamura@users.noreply.github.com>
2022-07-02 22:08:18 +0900

    f

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
         <p className="description">Get started by editing my bfig32<code>pages/index.js</code></p>
         <Link href="/about"><a>About</a></Link>
         <SideHeader title="Side Header" />
-        <Image src="./public/images/Sofie-Chair-White-Iso-copy.png" height={1540} width={978} alt="TheStormThatIsApproaching"/>
+        <Image src="public/images/Sofie-Chair-White-Iso-copy.png" alt="TheStormThatIsApproaching"/>
       </main>
     </div>
 
