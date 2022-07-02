b59564de8ae18fb1d832670447f85e20003e4168
Pixy <73171585+AkioHamamura@users.noreply.github.com>
2022-07-02 16:53:36 +0900

    f

--- components/SideHeader.js
+++ components/SideHeader.js
@@ -2,7 +2,7 @@ import styles from './SideHeaderStyles.module.css';
 
 export default function SideHeader({}) {
   return (
-    <ul className="side-header">
+    <ul class='ss'>
       <li><a href="/about">About</a></li>
       <li><a href="/contact">Contact</a></li>
       <li><a href="/blog">Blog</a></li>
