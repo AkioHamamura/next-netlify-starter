07384348f153d2bb30fe29538848d2a9dde80bb5
Pixy <73171585+AkioHamamura@users.noreply.github.com>
2022-07-02 23:05:12 +0900

    div => span

--- pages/Headertest.js
+++ pages/Headertest.js
@@ -2,8 +2,9 @@ import Link from 'next/link';
 
 export default function HeaderTest() {
   return (
-    <div>
+    <span>
+     
       <Link href="/"><a><h1>About me</h1></a></Link><Link href="/"><a><h1>About me2</h1></a></Link><Link href="/"><a><h1>About me3</h1></a></Link>
-    </div>
+    </span>
   );
 }
