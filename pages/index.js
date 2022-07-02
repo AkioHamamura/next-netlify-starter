5747a934fe873113c59f9636d0cc6a545f0ed9b1
Pixy <73171585+AkioHamamura@users.noreply.github.com>
2022-07-02 13:38:25 +0900

    123

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 import Link from 'next/link'
-
+import SideHeader from '../components/SideHeader'
 export default function Home() {
   return (
     <div className="container">
@@ -15,6 +15,7 @@ export default function Home() {
         <Header title="Welcome to my app!" />
         <p className="description">Get started by editing my bfig32<code>pages/index.js</code></p>
         <Link href="/about"><a>About</a></Link>
+        <SideHeader/>
       </main>
     </div>
 
