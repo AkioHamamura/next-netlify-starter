f2d04f951f4d05310844abcded7fda745cabe9c9
Pixy <73171585+AkioHamamura@users.noreply.github.com>
2022-07-02 21:49:54 +0900

    123

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Footer from '@components/Footer'
 import Link from 'next/link'
 import SideHeader from '@components/SideHeader'
 import { Fragment } from 'react'
+import { Image } from 'next/image'
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
         <p className="description">Get started by editing my bfig32<code>pages/index.js</code></p>
         <Link href="/about"><a>About</a></Link>
         <SideHeader title="Side Header" />
+        <Image src="public/images/Sofie-Chair-White-Iso-copy.png"></Image>
       </main>
     </div>
 
