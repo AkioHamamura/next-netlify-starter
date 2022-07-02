8521a7549618623b0e98b501951405189bad3d9e
Pixy <73171585+AkioHamamura@users.noreply.github.com>
2022-07-02 23:18:15 +0900

    123

--- pages/Headertest.js
+++ pages/Headertest.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function HeaderTest() {
      return (
      //make the div element inline so it can be styled
-     <div className="header">
+     <div className="header1">
      
       <Link href="/"><a><h1>About me</h1></a></Link><Link href="/"><a><h1>About me2</h1></a></Link><Link href="/"><a><h1>About me3</h1></a></Link>
 
@@ -12,10 +12,14 @@ export default function HeaderTest() {
         .container {
           margin: 50px;
         }
-        .header {
+        .header1 {
           color: blue;
           display: inline;
         }
+        .Header1 {
+          color: red;
+          display: inline;
+     }
       `}</style>
 
     </div>
