e35aa31e707657776979866331b906a580fe1f9a
Pixy <73171585+AkioHamamura@users.noreply.github.com>
2022-07-02 23:15:48 +0900

    asdf

--- pages/Headertest.js
+++ pages/Headertest.js
@@ -1,10 +1,22 @@
 import Link from 'next/link';
 
+
 export default function HeaderTest() {
-  return (
-    <span>
+     return (
+     //make the div element inline so it can be styled
+     <div className="header">
      
       <Link href="/"><a><h1>About me</h1></a></Link><Link href="/"><a><h1>About me2</h1></a></Link><Link href="/"><a><h1>About me3</h1></a></Link>
-    </span>
-  );
-}
+
+      <style jsx>{`
+        .container {
+          margin: 50px;
+        }
+        .header {
+          color: blue;
+          display: inline;
+        }
+      `}</style>
+
+    </div>
+  )}
